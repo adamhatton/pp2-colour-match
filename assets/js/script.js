ab5267139f3e0913760c55cd555982e56b325f38
adamhatton <adamdhatton@gmail.com>
2022-02-25 19:52:42 +0000

    Add styles to game area and code to hide menu and show game area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let rulesBtn = document.getElementById('rules-btn');
 
+    let playBtn = document.getElementById('play-btn');
+    playBtn.addEventListener('click', function(){
+        document.getElementsByClassName('game-area')[0].style.display = 'flex';
+        document.getElementById('menu').style.display = 'none';
+    })
+    
+    
+    //Show rules modal
+    let rulesBtn = document.getElementById('rules-btn');
     rulesBtn.addEventListener('click', function() {
         document.getElementsByClassName('rules-modal')[0].style.display = 'flex';
     })
 
+    //Hide rules modal
     let closeBtn = document.getElementsByClassName('close-btn')[0];
-
     closeBtn.addEventListener('click', function() {
         document.getElementsByClassName('rules-modal')[0].style.display = 'none';
     })
 
 })
 
-//TODO Move open and close modal into a single function
