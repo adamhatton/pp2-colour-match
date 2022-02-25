30930917ea79d1d9b96685b04219c6993db44cbe
adamhatton <adamdhatton@gmail.com>
2022-02-25 23:35:37 +0000

    Add code for starting game and creating colour array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+let gameSequence = [];
+let buttonsActive = false;
+
 document.addEventListener('DOMContentLoaded', function() {
 
     //Show game area and hide menu
@@ -26,5 +29,51 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementsByClassName('rules-modal')[0].style.display = 'none';
     })
 
+    //Start new game
+    let startBtn = document.getElementById('start-btn');
+    startBtn.addEventListener('click', function() {
+        startGame();
+    })
 })
 
+function startGame() {
+    gameSequence = [];
+    addRandomColour();
+    addRandomColour();
+    addRandomColour();
+    console.log(gameSequence);
+    showSequence();
+}
+
+function addRandomColour() {
+    let colour;
+    let colourNumber = Math.ceil(Math.random()*4);
+
+    switch (colourNumber) {
+        case 1:
+            colour = 'red';
+            break;
+        case 2:
+            colour = 'green';
+            break;
+        case 3:
+            colour = 'blue';
+            break;
+        case 4:
+            colour = 'yellow';
+            break;
+    }
+    gameSequence.push(colour);
+}
+
+function showSequence() {
+    let i = 1;
+
+    for (let colour of gameSequence){
+        setTimeout(function () {
+            document.getElementById(`${colour}-btn`).style.opacity = 1;
+         }, 500 * i);
+         i++;
+
+    }
+}
